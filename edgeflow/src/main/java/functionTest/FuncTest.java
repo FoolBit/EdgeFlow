afc15361cf613da97d73a2a18afb2a1ead9f3c85
BinTang <tangbin1998@pku.edu.cn>
2019-10-06 22:59:21 +0800

    fix bugs

---
+++ edgeflow/src/main/java/functionTest/FuncTest.java
@@ -0,0 +1,15 @@
+package functionTest;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+import utils.SysUtils;
+
+public class FuncTest {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(System.currentTimeMillis());
+
+    }
+}
+
